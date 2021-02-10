074d336119b12fca03bb3a187a598ab42abb2294
CoderEx24 <kareemhacker123321@gmail.com>
2021-02-10 22:06:32 +0200

    some modifications

--- train_model.py
+++ train_model.py
@@ -4,10 +4,12 @@ import pandas as pd
 
 df = pd.read_csv('./training_dataset.csv')
 
-x = np.array(df['symptoms']).reshape((-1, 1))
+MAX = 2**133 - 1
+
+x = [ int(i) / MAX for i in np.array(df['symptoms'])]
 y = np.array(df['prognosis'])
 
-x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.2)
+x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.3)
 
 model = neighbors.KNeighborsClassifier()
 model.fit(x_train, y_train)
